ad6a333c9997aa8c95d295f17076a21c118d31c7
Asadmau <49995561+asadmau@users.noreply.github.com>
2021-03-19 02:33:32 +0700

    Update .gitignore

---
+++ main.py
@@ -0,0 +1,16 @@
+# type data dict => type data dictionary
+# merupakan type data yang dapat menyimpan data dalam bentuk pasangan nilai-kunci
+
+
+mahasiswa = {
+    'nim' : 1117101481,
+    'nama_mahasiswa' : 'asad mau',
+    'alamat' : 'jember',
+    'email' : 'asadbaen@gmail.com',
+    'password' : '45417naa'
+}
+mahasiswa['wa'] = '08319238124'
+print(mahasiswa)
+print('nim mahasiswa :', mahasiswa['nim'])
+print('alamat mahasiswa :', mahasiswa['alamat'])
+print('No Wa mahasiswa :', mahasiswa['wa'])
